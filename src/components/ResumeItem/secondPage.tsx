1770fdf2fd3ca65eab21e1c257444ad8d05f78dd
littleforest <670177821@qq.com>
2020-04-28 18:41:28 +0800

    resumepagesecond 完成

--- src/components/ResumeItem/secondPage.tsx
+++ src/components/ResumeItem/secondPage.tsx
@@ -11,7 +11,7 @@ import QueueAnim from 'rc-queue-anim';
 const SecondResume = ()=>{
     return (
         <div className="frist-img middle">
-            <div className="one resume-page">
+            <div className="two resume-page">
                 <TweenOne className="banner-user-title" 
                     animation={{ y: 30, opacity: 0, type: 'from' }}
                     id="title"
@@ -36,7 +36,46 @@ const SecondResume = ()=>{
                             CSS
                         </div>
                     </div>
+                    <div className="second-round">
+                        <div className="round2-item  round-Ts">
+                            Ts
+                        </div>
+                        <div className="round2-item round-Node">
+                            Node
+                        </div>
+                        <div className="round2-item round-Vue">
+                            Vue
+                        </div>
+                        <div className="round2-item round-React">
+                            React
+                        </div>
+                        <div className="round2-item round-Flutter">
+                            Flutter
+                        </div>
+                        <div className="round2-item round-LESS">
+                            LESS
+                        </div>
+                        <div className="round2-item round-Express">
+                            Express
+                        </div>
+                        <div className="round2-item round-Webpack">
+                            Webpack
+                        </div>
+                    </div>
                 </div>
+                <QueueAnim>
+                    <div key="1">
+                    熟练使用HTML/CSS技术，熟悉Javascript/Typescript编程,能够熟练使用reactjs等框架进行模块开发，
+                  
+                    </div>
+                    <div key="2">
+                    熟练的使用webpack等前端工具，了解less，sass等前端新技术,
+                    </div>
+                    <div key="3">
+                    熟悉Flutter开发手机APP
+                    </div>
+                </QueueAnim>
+               
             </div>
         </div>
     )
